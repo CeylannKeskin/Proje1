f31112dd34ac04dac5ef7fdcbe8b76982ba42fb9
CeylannKeskin <131559495+CeylannKeskin@users.noreply.github.com>
2023-04-23 05:10:28 +0300

    Proje

---
+++ Project-1-Calculator/Project-1-Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Project_1_Calculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HESAP MAKİNASI");
+
+        int toplam = 0, cıkarma = 0, carpma = 0, bolme = 0;
+
+        Console.WriteLine("Bir sayı girin");
+        int sayi1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Bir sayı girin");
+        int sayi2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Lütfen yapmak istediğiniz işlemi girin! ('Toplama: + ,Çıkarma: - ,Çarpma: x ,Bölme: / ')");
+
+        char islem = char.Parse(Console.ReadLine());
+
+        if (sayi2 == 0)
+        {
+            Console.WriteLine("Sayı 0 olamaz");
+            return;
+        }
+        else
+        {
+
+        }
+        switch (islem)
+        {
+
+            case ('+'):
+                toplam = sayi1 + sayi2;
+                Console.WriteLine(toplam);
+                break;
+            case ('-'):
+                cıkarma = sayi1 - sayi2;
+                Console.WriteLine(cıkarma);
+                break;
+            case ('x'):
+                carpma = sayi1 * sayi2;
+                Console.WriteLine(carpma);
+                break;
+            case ('/'):
+                bolme = sayi1 / sayi2;
+                Console.WriteLine(bolme);
+                break;
+
+
+        }
+
+    }
+}
+
